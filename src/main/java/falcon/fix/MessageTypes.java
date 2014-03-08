6f709a03dfa64332d63f0d626154cc1567ccde92
Pekka Enberg <penberg@iki.fi>
2014-03-08 14:54:35 +0200

    Fix formatting in MessageTypes class
    Signed-off-by: Pekka Enberg <penberg@iki.fi>

--- src/main/java/falcon/fix/MessageTypes.java
+++ src/main/java/falcon/fix/MessageTypes.java
@@ -2,14 +2,14 @@ package falcon.fix;
 
 public class MessageTypes {
 
-    public static final MessageType Heartbeat      = new MessageType("0");
-    public static final MessageType TestRequest    = new MessageType("1");
-    public static final MessageType ResendRequest  = new MessageType("2");
-    public static final MessageType Reject         = new MessageType("3");
-    public static final MessageType SequenceReset  = new MessageType("4");
-    public static final MessageType Logout         = new MessageType("5");
-    public static final MessageType ExecutionReport= new MessageType("8");
-    public static final MessageType Logon          = new MessageType("A");
-    public static final MessageType NewOrderSingle = new MessageType("D");
+  public static final MessageType Heartbeat      = new MessageType("0");
+  public static final MessageType TestRequest    = new MessageType("1");
+  public static final MessageType ResendRequest  = new MessageType("2");
+  public static final MessageType Reject         = new MessageType("3");
+  public static final MessageType SequenceReset  = new MessageType("4");
+  public static final MessageType Logout         = new MessageType("5");
+  public static final MessageType ExecutionReport= new MessageType("8");
+  public static final MessageType Logon          = new MessageType("A");
+  public static final MessageType NewOrderSingle = new MessageType("D");
 
 }
